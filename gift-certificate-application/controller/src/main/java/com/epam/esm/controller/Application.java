6cbd1b3596401bdb0a9a96d7574821cbe6f937ea
YuryLapitski <yura.lapitski@gmail.com>
2022-06-21 14:05:51 +0300

    App deploy in Tomcat

--- gift-certificate-application/controller/src/main/java/com/epam/esm/controller/Application.java
+++ gift-certificate-application/controller/src/main/java/com/epam/esm/controller/Application.java
@@ -3,13 +3,20 @@ package com.epam.esm.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
 @EntityScan("com.epam.esm")
 @EnableJpaRepositories("com.epam.esm")
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
